eaf683a24b08dcc599db060dd820f96a915e2117
uxin603s <uxin603s@gmail.com>
2016-12-25 16:49:05 +0800

    update

--- UserSystemHelp.php
+++ UserSystemHelp.php
@@ -1,6 +1,6 @@
 <?php
 class UserSystemHelp{
-	public static function login($success="",$cache="",$error="",$location=true){
+	public static function login($success="",$Fcache="",$error="",$location=true){
 		if(isset($_GET['access_token'])){
 			if(mb_strlen($_GET['access_token'])==32 && preg_match("/^[a-z0-9]+$/",$_GET['access_token'])){
 				$ip=$_SERVER['REMOTE_ADDR'];
@@ -14,9 +14,9 @@ class UserSystemHelp{
 				}
 				if($http_code!="200"){		
 					$result=[];
-					if(is_callable($cache)){
+					if(is_callable($Fcache)){
 						
-						if($tmp=call_user_func($cache,$_GET['access_token'])){
+						if($tmp=call_user_func($Fcache,$_GET['access_token'])){
 							$result['status']=true;
 							$result['data']=$tmp;
 							$result['message']="主站掛點使用快取";
@@ -43,7 +43,13 @@ class UserSystemHelp{
 				call_user_func($error,$result['message']);
 			}
 			
-		}else{
+		}
+		elseif(isset($_GET['error'])){
+			if(is_callable($error)){
+				call_user_func($error,$result['error']);
+			}
+		}
+		else{
 			$go_to=urlencode("http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}");
 			header("location: http://user.cfd888.info/login.php?goto={$go_to}");
 		}
@@ -56,7 +62,7 @@ class UserSystemHelp{
 		$_SESSION=$data;
 		$data['session_id']=session_id();
 		//data寫入快取方便主站掛點時利用 及主站 刷新外部網站用
-		Cache::set("userSystem_{$access_token}",$data,60*30);
+		Fcache::set("userSystem_{$access_token}",$data,60*30);
 		session_write_close();
 		//data找導頁資料並導頁
 		if($location){
@@ -64,9 +70,9 @@ class UserSystemHelp{
 		}
 		
 	}
-	public static function cache($access_token){
+	public static function Fcache($access_token){
 		//取得data快取return
-		return Cache::get("userSystem_{$access_token}");
+		return Fcache::get("userSystem_{$access_token}");
 	}
 	public static function error($message){
 		// 錯誤頁面顯示
@@ -74,13 +80,13 @@ class UserSystemHelp{
 	}
 	public static function flushData(){
 		if(in_array($_SERVER['REMOTE_ADDR'],["192.168.1.1"])){
-			$list=Cache::where("userSystem_");
+			$list=Fcache::where("userSystem_");
 			//找access_token與session_id關聯
 			//並重新回主站要資料重新寫入
 			foreach($list as $item){
 				session_id($item['session']);
 				$_GET['access_token']=$item['access_token'];
-				self::login("UserSystemHelp::success","UserSystemHelp::cache","UserSystemHelp::error",false);
+				self::login("UserSystemHelp::success","UserSystemHelp::Fcache","UserSystemHelp::error",false);
 			}
 			
 		}
