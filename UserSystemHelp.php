8a2b0c5bc78674e591cce6cecaaf14d8b1db686e
uxin603s <uxin603s@gmail.com>
2016-12-26 17:36:42 +0800

    update

--- UserSystemHelp.php
+++ UserSystemHelp.php
@@ -1,7 +1,8 @@
 <?php
 class UserSystemHelp{
-	public static function login($success="",$Fcache="",$error="",$location=true){
+	public static function login($success="",$Mcache="",$error="",$location=true){
 		if(isset($_GET['access_token'])){
+		
 			if(mb_strlen($_GET['access_token'])==32 && preg_match("/^[a-z0-9]+$/",$_GET['access_token'])){
 				$ip=$_SERVER['REMOTE_ADDR'];
 				$url="http://user.cfd888.info/api.php?access_token={$_GET['access_token']}&ip={$ip}";
@@ -14,9 +15,9 @@ class UserSystemHelp{
 				}
 				if($http_code!="200"){		
 					$result=[];
-					if(is_callable($Fcache)){
+					if(is_callable($Mcache)){
 						
-						if($tmp=call_user_func($Fcache,$_GET['access_token'])){
+						if($tmp=call_user_func($Mcache,$_GET['access_token'])){
 							$result['status']=true;
 							$result['data']=$tmp;
 							$result['message']="主站掛點使用快取";
@@ -51,28 +52,41 @@ class UserSystemHelp{
 		}
 		else{
 			$go_to=urlencode("http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}");
-			header("location: http://user.cfd888.info/login.php?goto={$go_to}");
+			header("location: http://user.cfd888.info/login.php?go_to={$go_to}");
 		}
 	}
 	public static function success($data,$location){
+		
 		$access_token=$data['access_token'];
-		$go_to=$data['go_to'];
-		session_start();
+		
+		
+		if($data['go_to'] && is_numeric(strpos("http://{$_SERVER['HTTP_HOST']}",$data['go_to']))){
+			$go_to=$data['go_to'];
+		}else{
+			$go_to="/index.php";
+		}
+			
+		// echo "<pre>";
+		// var_dump($go_to);
+		// exit;
+		
 		//寫入session
+		session_start();
 		$_SESSION=$data;
 		$data['session_id']=session_id();
-		//data寫入快取方便主站掛點時利用 及主站 刷新外部網站用
-		Fcache::set("userSystem_{$access_token}",$data,60*30);
 		session_write_close();
+		//data寫入快取方便主站掛點時利用 及主站 刷新外部網站用
+		Mcache::set("userSystem_{$access_token}",$data,60*30);
+		
 		//data找導頁資料並導頁
 		if($location){
 			header("location: {$go_to}");
 		}
 		
 	}
-	public static function Fcache($access_token){
+	public static function cache($access_token){
 		//取得data快取return
-		return Fcache::get("userSystem_{$access_token}");
+		return Mcache::get("userSystem_{$access_token}");
 	}
 	public static function error($message){
 		// 錯誤頁面顯示
@@ -81,15 +95,20 @@ class UserSystemHelp{
 	public static function flushData(){
 		$white_ip=json_decode(file_get_contents(__DIR__."/white_ip.json"),1);
 		if(in_array($_SERVER['REMOTE_ADDR'],$white_ip)){
-			$list=Fcache::where("userSystem_");
+			$list=Mcache::where("userSystem_");
 			//找access_token與session_id關聯
 			//並重新回主站要資料重新寫入
 			foreach($list as $item){
-				session_id($item['session']);
-				$_GET['access_token']=$item['access_token'];
-				self::login("UserSystemHelp::success","UserSystemHelp::Fcache","UserSystemHelp::error",false);
+				$access_token=$item['access_token'];
+				if($item['session_id']){
+					session_id($item['session_id']);
+					
+					$_GET['access_token']=$access_token;
+					self::login("UserSystemHelp::success","UserSystemHelp::cache","UserSystemHelp::error",false);
+				}else{
+					Mcache::del("userSystem_{$access_token}");
+				}
 			}
-			
 		}
 	}
 }
